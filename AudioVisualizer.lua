63e857a64a48be15791de21f3401e7ae326385b9
Sudospective <avery@sudospective.net>
2024-10-14 17:18:37 -0700

    Edit AudioVisualizer.lua

--- AudioVisualizer.lua
+++ AudioVisualizer.lua
@@ -1,6 +1,8 @@
 ---@meta
 
----@class AudioVisualizer
+---@class AudioVisualizer : Actor
+---@field [string] fun(self: AudioVisualizer)
+---@diagnostic disable: redundant-parameter
 AudioVisualizer = {}
 
 --- Returns the quads used for the visualizer. This can be used to
