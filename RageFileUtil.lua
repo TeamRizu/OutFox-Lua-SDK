3c403dd578f835b5c925b16a8fc5d16ca0b7add7
Sudospective <avery@sudospective.net>
2024-10-19 12:33:16 -0700

    Add RageFileUtil.lua

---
+++ RageFileUtil.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@class RageFileUtil
+RageFileUtil = {}
+
+--- Creates a RageFile handle with which one can use the commands in `RageFile`.
+---@return RageFile
+function RageFileUtil.CreateRageFile() end
