2bea90bcc86c459cfad2378795f35ebaf7c61ea8
Sudospective <avery@sudospective.net>
2024-10-18 19:00:03 -0700

    Add CourseContentsList.lua

---
+++ CourseContentsList.lua
@@ -0,0 +1,9 @@
+---@meta
+
+---@class CourseContentsList : ActorScroller
+---@overload fun(self: CourseContentsList) : self
+---@field [string] fun(self: CourseContentsList)
+CourseContentsList = {}
+
+--- Sets the CourseContentsList from the GameState.
+function CourseContentsList:SetFromGameState() end
