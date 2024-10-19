e9289f7c99743e71a6a94887e0fdc7337df7705b
Sudospective <avery@sudospective.net>
2024-10-19 10:30:52 -0700

    Add other LifeMeters to LifeMeter.lua

--- LifeMeter.lua
+++ LifeMeter.lua
@@ -21,3 +21,42 @@ function LifeMeter:IsHot() end
 --- Returns `true` if in danger.
 ---@return boolean
 function LifeMeter:IsInDanger() end
+
+
+---@class LifeMeterBattery : LifeMeter
+---@overload fun(self: LifeMeterBattery): self
+---@field [string] fun(self: LifeMeterBattery)
+LifeMeterBattery = {}
+
+--- Changes the player's life by `iNumLives`. (Negative values subtract lives.)
+---@param iNumLives integer
+function LifeMeterBattery:ChangeLives(iNumLives) end
+
+--- Returns the number of lives remaining.
+---@return integer
+function LifeMeterBattery:GetLivesLeft() end
+
+--- Returns the number of total lives.
+---@return integer
+function LifeMeterBattery:GetTotalLives() end
+
+
+---@class LifeMeterTime : LifeMeter
+---@overload fun(self: LifeMeterTime): self
+---@field [string] fun(self: LifeMeterTime)
+LifeMeterTime = {}
+
+--- Substracts `delta` from the player's current time. (Insert the opposite to
+--- increase its value.)
+function LifeMeterTime:ChangeTime(delta) end
+
+--- Returns the current life of the player from 0 to 1.
+---@return integer
+function LifeMeterTime:GetLife() end
+
+--- Returns the seconds equivalent of GetLife, by reporting how time is left for the player.
+---@return number
+function LifeMeterTime:GetLifeSeconds() end
+
+--- Sets the time left for the player by `value`.
+function LifeMeterTime:SetLife(value) end
