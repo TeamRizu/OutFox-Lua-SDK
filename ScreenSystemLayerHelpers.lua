37920d38ca8dce83600c61f9b357045a2a15bfdc
Sudospective <avery@sudospective.net>
2024-10-20 12:35:26 -0700

    Add ScreenSystemLayerHelpers.lua

---
+++ ScreenSystemLayerHelpers.lua
@@ -0,0 +1,9 @@
+---@meta
+
+---@class ScreenSystemLayerHelpers
+ScreenSystemLayerHelpers = {}
+
+--- Gets the credits message for Player `pn`.
+---@param pn PlayerNumber
+---@return string
+function ScreenSystemLayerHelpers.GetCreditsMessage(pn) end
